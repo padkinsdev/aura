f904ba03881837f85b9ef28127b11fbfdc22186c
Kate Adkins <50161615+padkinsdev@users.noreply.github.com>
2020-08-03 17:03:57 -0400

    Update file_upload_parser.js

--- utils/file_upload_parser.js
+++ utils/file_upload_parser.js
@@ -10,32 +10,20 @@ function parseForm(data){
     // SO MUCH NESTING!!!
     let files = [];
     let chunks = data.split('------WebKitFormBoundary');
-    //console.log(chunks);
-    //return;
     chunks.forEach((value, index) => {
-        //console.log("Value: "+value);
         let fnamepos = value.search('filename');
         if (fnamepos < 0){
-            //console.log("No filename");
             files.push(new UploadedFile("", "", "No filename attribute"));
         } else {
             let lines = value.split('\n');
-            //console.log("Lines:");
-            //console.log("Lines[1]: "+lines[1]);
             let headers = lines[1].split(";");
             if (headers.length != 3){
-                //console.log("Wrong # of headers");
                 files.push(new UploadedFile("", "", "Wrong number of headers"));
             } else {
                 let filename = headers[2].split('=')[1];
                 filename = filename.slice(1, filename.length-2);
-                //console.log("Filename: "+filename);
-                //lines.splice(0,4);
                 lines.splice(0,4); // remove the form data headers
                 let fdata = lines.join("\n");
-                //console.log("data:");
-                //console.log(fdata);
-                //console.log("-----------");
                 files.push(new UploadedFile(filename, fdata.replace(/%20/g, " "), ""));
             }
         }
